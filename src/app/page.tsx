4142f6ad63b7cd86eb0c1a89cd39f17b8c5d96b9
jolene-wang <jcw2239@columbia.edu>
2026-02-20 12:37:35 -0500

    cleaning code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,14 +63,11 @@ export default function Home() {
         setError(error.message);
       } else if (data) {
         const captionIds = data.map(c => c.id);
-        const { data: votes, error: votesError } = await supabase
+        const { data: votes } = await supabase
           .from("caption_votes")
           .select("caption_id, vote_value")
           .eq("profile_id", user.id)
           .in("caption_id", captionIds);
-        
-        console.log('Fetched votes:', votes);
-        console.log('Votes error:', votesError);
         
         const captionsWithVotes = data.map(caption => ({
           ...caption,
